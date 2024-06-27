7f5a33ba10f03cc4ba9016dcdc31c87cb5d818cd
TJMusiitwa <jonamusiitwa@outlook.com>
2024-06-27 21:59:59 +0300

    Revert css file removal

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ void main() {
     title: 'CU Apps',
     lang: 'en',
     head: [
-      link(href: 'styles.css', rel: 'stylesheet'),
+      link(href: 'https://cdn.tailwindcss.com', rel: 'stylesheet'),
+      script([], src: 'https://cdn.tailwindcss.com'),
       script([],
           defer: true,
           src: 'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js')
