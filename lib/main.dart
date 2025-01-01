9a670ce1e63de6c3471cb18e6865f683b2def6fd
TJMusiitwa <jonamusiitwa@outlook.com>
2025-01-01 11:25:51 +0300

    🐛 Comment out meta tags in main.dart to prevent SEO conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,16 @@ void main() {
     lang: 'en',
     charset: 'UTF-8',
     viewport: 'width=device-width, initial-scale=1.0',
-    meta: {
-      'description':
-          'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
-      'author': 'CU Apps',
-      'keywords': 'CU Apps, Mobile Apps, Financial Mutuals, Bespoke Apps',
-      'og:title': 'CU Apps',
-      'og:description':
-          'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
-      'og:image': 'https://cuapps.co.uk/assets/images/cuapps-logo.png',
-    },
+    // meta: {
+    //   'description':
+    //       'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
+    //   'author': 'CU Apps',
+    //   'keywords': 'CU Apps, Mobile Apps, Financial Mutuals, Bespoke Apps',
+    //   'og:title': 'CU Apps',
+    //   'og:description':
+    //       'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
+    //   'og:image': 'https://cuapps.co.uk/assets/images/cuapps-logo.png',
+    // },
     head: [
       // link(href: 'https://cdn.tailwindcss.com'),
       // script([], src: 'https://cdn.tailwindcss.com'),
