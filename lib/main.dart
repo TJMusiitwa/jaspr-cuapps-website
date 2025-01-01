773d07972ab90ad6a81b5b67e4bfd1f21fccae21
TJMusiitwa <jonamusiitwa@outlook.com>
2025-01-01 11:25:23 +0300

    ✨ Enhance Document metadata in main.dart for improved SEO and accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,27 @@ import 'jaspr_options.dart';
 import 'styles.dart';
 
 void main() {
-  Jaspr.initializeApp(
-    options: defaultJasprOptions,
-  );
+  Jaspr.initializeApp(options: defaultJasprOptions);
 
   runApp(Document(
     title: 'CU Apps',
     lang: 'en',
+    charset: 'UTF-8',
+    viewport: 'width=device-width, initial-scale=1.0',
+    meta: {
+      'description':
+          'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
+      'author': 'CU Apps',
+      'keywords': 'CU Apps, Mobile Apps, Financial Mutuals, Bespoke Apps',
+      'og:title': 'CU Apps',
+      'og:description':
+          'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
+      'og:image': 'https://cuapps.co.uk/assets/images/cuapps-logo.png',
+    },
     head: [
-      link(href: 'https://cdn.tailwindcss.com'),
-      script([], src: 'https://cdn.tailwindcss.com'),
+      // link(href: 'https://cdn.tailwindcss.com'),
+      // script([], src: 'https://cdn.tailwindcss.com'),
+      link(href: 'styles.css', rel: 'stylesheet'),
       script([],
           defer: true,
           src: 'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js')
