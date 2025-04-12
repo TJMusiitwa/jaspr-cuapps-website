4511ae091a48babce5fc31403872f192f0c650a2
TJMusiitwa <jonamusiitwa@outlook.com>
2025-04-12 21:12:41 +0300

    ✨ Update main.dart to use DaisyUI version 5 and Tailwind CSS browser script for improved styling

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,31 @@ import 'jaspr_options.dart';
 void main() {
   Jaspr.initializeApp(options: defaultJasprOptions);
 
-  runApp(Document(
-    title: 'CU Apps',
-    lang: 'en',
-    charset: 'UTF-8',
-    viewport: 'width=device-width, initial-scale=1.0',
-    // meta: {
-    //   'description':
-    //       'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
-    //   'author': 'CU Apps',
-    //   'keywords': 'CU Apps, Mobile Apps, Financial Mutuals, Bespoke Apps',
-    //   'og:title': 'CU Apps',
-    //   'og:description':
-    //       'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
-    //   'og:image': 'https://cuapps.co.uk/assets/images/cuapps-logo.png',
-    // },
-    head: [
-      link(href: 'styles.css', rel: 'stylesheet'),
-      link(
-          href:
-              'https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css',
+  runApp(
+    Document(
+      title: 'CU Apps',
+      lang: 'en',
+      charset: 'UTF-8',
+      viewport: 'width=device-width, initial-scale=1.0',
+      // meta: {
+      //   'description':
+      //       'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
+      //   'author': 'CU Apps',
+      //   'keywords': 'CU Apps, Mobile Apps, Financial Mutuals, Bespoke Apps',
+      //   'og:title': 'CU Apps',
+      //   'og:description':
+      //       'CU Apps offer Premium and Affordable Bespoke Mobile Apps for Financial Mutuals',
+      //   'og:image': 'https://cuapps.co.uk/assets/images/cuapps-logo.png',
+      // },
+      head: [
+        link(
+          href: 'https://cdn.jsdelivr.net/npm/daisyui@5',
           rel: 'stylesheet',
-          type: 'text/css'),
-      script(src: 'https://cdn.tailwindcss.com', []),
-    ],
-    body: App(),
-  ));
+          type: 'text/css',
+        ),
+        script(src: 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4', []),
+      ],
+      body: App(),
+    ),
+  );
 }
