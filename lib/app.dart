d16ab398efe59398a9b69fdbfdd91ada748298ef
TJMusiitwa <jonamusiitwa@outlook.com>
2025-09-17 22:26:29 +0300

    Refactor: Simplify build method in App component for improved readability

--- lib/app.dart
+++ lib/app.dart
@@ -14,46 +14,48 @@ import 'pages/home.dart';
 @client
 class App extends StatelessComponent {
   @override
-  Iterable<Component> build(BuildContext context) sync* {
-    yield div(attributes: {
-      'data-theme': 'winter'
-    }, [
-      Router(
-        routes: [
-          ShellRoute(
-            builder: (context, state, child) => Fragment(children: [
-              Banner(),
-              Header(),
-              child,
-              Footer(),
-            ]),
-            routes: [
-              Route(
+  Component build(BuildContext context) {
+    return div(
+      attributes: {'data-theme': 'winter'},
+      [
+        Router(
+          routes: [
+            ShellRoute(
+              builder: (context, state, child) =>
+                  fragment([Banner(), Header(), child, Footer()]),
+              routes: [
+                Route(
                   path: '/',
                   title: 'Home',
-                  builder: (context, state) => const Home()),
-              Route(
+                  builder: (context, state) => const Home(),
+                ),
+                Route(
                   path: '/app-features',
                   title: 'Features',
-                  builder: (context, state) => AppFeatures()),
-              Route(
+                  builder: (context, state) => AppFeatures(),
+                ),
+                Route(
                   path: '/about',
                   title: 'About',
-                  builder: (context, state) => const About()),
-              Route(
+                  builder: (context, state) => const About(),
+                ),
+                Route(
                   path: '/contact-us',
                   title: 'Contact Us',
-                  builder: (context, state) => ContactUs()),
-              Route(
+                  builder: (context, state) => ContactUs(),
+                ),
+                Route(
                   path: '/free-demo',
                   title: 'Free Demo',
-                  builder: (context, state) => FreeDemo()),
-            ],
-          ),
-          Route(path: '/*', builder: (context, state) => Error404()),
-        ],
-        errorBuilder: (context, state) => Error404(),
-      ),
-    ]);
+                  builder: (context, state) => FreeDemo(),
+                ),
+              ],
+            ),
+            Route(path: '/*', builder: (context, state) => Error404()),
+          ],
+          errorBuilder: (context, state) => Error404(),
+        ),
+      ],
+    );
   }
 }
