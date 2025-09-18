09f1a77a592547d8464c78e848eb882fe3820dd0
TJMusiitwa <jonamusiitwa@outlook.com>
2025-09-18 03:45:28 +0300

    Refactor: Change build method in FreeDemo component to return a Component instead of using sync* for improved clarity

--- lib/pages/free_demo.dart
+++ lib/pages/free_demo.dart
@@ -2,8 +2,8 @@ import 'package:jaspr/jaspr.dart';
 
 class FreeDemo extends StatelessComponent {
   @override
-  Iterable<Component> build(BuildContext context) sync* {
-    yield div(classes: 'relative bg-gradient-to-r to-[rgba(0,105,255,1)] from-[#cee4ff]', [
+  Component build(BuildContext context) {
+    return div(classes: 'relative bg-gradient-to-r to-[rgba(0,105,255,1)] from-[#cee4ff]', [
       div(classes: 'max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto', [
         div(classes: 'grid items-center md:grid-cols-2 gap-8 lg:gap-12', [
           div([
